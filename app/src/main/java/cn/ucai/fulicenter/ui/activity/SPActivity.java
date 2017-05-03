686ef82af51c41548dbe5e7da09b6a84c85541e8
wuxiaofei <761241917@qq.com>
2017-05-03 14:20:21 +0800

    实现闪屏

--- app/src/main/java/cn/ucai/fulicenter/ui/activity/SPActivity.java
+++ app/src/main/java/cn/ucai/fulicenter/ui/activity/SPActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import cn.ucai.fulicenter.R;
@@ -26,6 +27,10 @@ public class SPActivity extends AppCompatActivity {
         super.onResume();
         timer=new MyCountTimer(time,1000);
         timer.start();
+    }
+
+    public void on(View view) {
+        startActivity(new Intent(SPActivity.this,MainActivity.class));
     }
 
     class MyCountTimer extends CountDownTimer {
