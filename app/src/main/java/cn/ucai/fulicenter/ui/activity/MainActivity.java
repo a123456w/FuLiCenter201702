42ae4a38232ce44e85e57dd7508e313306ee95d6
wuxiaofei <761241917@qq.com>
2017-05-04 10:10:22 +0800

    写入网络权限，测试下载数据

--- app/src/main/java/cn/ucai/fulicenter/ui/activity/MainActivity.java
+++ app/src/main/java/cn/ucai/fulicenter/ui/activity/MainActivity.java
@@ -2,9 +2,14 @@ package cn.ucai.fulicenter.ui.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 
 import cn.ucai.fulicenter.R;
+import cn.ucai.fulicenter.data.bean.NewGoodsBean;
+import cn.ucai.fulicenter.data.net.DownNewGoodMode;
+import cn.ucai.fulicenter.data.net.IDownNewGood;
+import cn.ucai.fulicenter.data.utils.OkHttpUtils;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,7 +19,26 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
     public void onCheckedChange(View v){
-
+        textDownload();
     }
+    void textDownload(){
+        IDownNewGood newgood=new DownNewGoodMode();
+        newgood.DownNewGoodData(MainActivity.this, 0, 1, 10,
+                new OkHttpUtils.OnCompleteListener<NewGoodsBean[]>() {
+                    @Override
+                    public void onSuccess(NewGoodsBean[] result) {
+                        Log.i("main" ,"result="+result);
+                        if(result!=null){
+                            for (NewGoodsBean bean : result) {
+                                Log.i("main" ,bean.toString());
+                            }
+                        }
+                    }
 
+                    @Override
+                    public void onError(String error) {
+
+                    }
+                });
+    }
 }
