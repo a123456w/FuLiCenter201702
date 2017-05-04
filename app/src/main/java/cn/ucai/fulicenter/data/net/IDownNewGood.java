71b564b8c4ad19e91f8d8a453274b10c4fd23247
wuxiaofei <761241917@qq.com>
2017-05-04 09:57:04 +0800

    添加net目录 编写IDownNewGood接口及其实现类

---
+++ app/src/main/java/cn/ucai/fulicenter/data/net/IDownNewGood.java
@@ -0,0 +1,16 @@
+package cn.ucai.fulicenter.data.net;
+
+import android.content.Context;
+import android.view.ViewTreeObserver;
+
+import cn.ucai.fulicenter.data.bean.NewGoodsBean;
+import cn.ucai.fulicenter.data.utils.OkHttpUtils;
+
+/**
+ * Created by Administrator on 2017/5/4 0004.
+ */
+
+public interface IDownNewGood {
+    void DownNewGoodData(Context context, int id, int pageid, int pageSize, OkHttpUtils.OnCompleteListener<NewGoodsBean[]> listener);
+
+}
