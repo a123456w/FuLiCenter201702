3cbbc1a67991609e98fcefb68f0a5973e3a57824
wuxiaofei <761241917@qq.com>
2017-05-05 21:29:30 +0800

    修改布局文件

--- app/src/main/java/cn/ucai/fulicenter/ui/activity/Goods2Activity.java
+++ app/src/main/java/cn/ucai/fulicenter/ui/activity/Goods2Activity.java
@@ -1,9 +1,12 @@
 package cn.ucai.fulicenter.ui.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import cn.ucai.fulicenter.R;
 import cn.ucai.fulicenter.application.I;
 import cn.ucai.fulicenter.data.bean.GoodsDetailsBean;
@@ -13,11 +16,24 @@ import cn.ucai.fulicenter.data.net.OnCompleteListener;
 public class Goods2Activity extends AppCompatActivity {
     DownNewGoodMode mode;
     int mGoodsid;
+
+    @BindView(R.id.tvEnglishName)
+    TextView tvEnglishName;
+    @BindView(R.id.tvName)
+    TextView tvName;
+    @BindView(R.id.tvCurrencyPrice)
+    TextView tvCurrencyPrice;
+    @BindView(R.id.tvBrief)
+    TextView tvBrief;
+    @BindView(R.id.activity_goods2)
+    LinearLayout activityGoods2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-                setContentView(R.layout.activity_goods2);
-        mGoodsid = getIntent().getIntExtra(I.Goods.KEY_GOODS_ID,I.CAT_ID);
+        setContentView(R.layout.activity_goods2);
+        ButterKnife.bind(this);
+        mGoodsid = getIntent().getIntExtra(I.Goods.KEY_GOODS_ID, I.CAT_ID);
         mode = new DownNewGoodMode();
         DownDetails();
     }
@@ -27,7 +43,7 @@ public class Goods2Activity extends AppCompatActivity {
         mode.DownGoodsDetails(this, mGoodsid, new OnCompleteListener<GoodsDetailsBean>() {
             @Override
             public void onSuccess(GoodsDetailsBean data) {
-
+                setView(data);
             }
 
             @Override
@@ -35,5 +51,12 @@ public class Goods2Activity extends AppCompatActivity {
 
             }
         });
+    }
+
+    private void setView(GoodsDetailsBean data) {
+        tvEnglishName.setText(data.getGoodsEnglishName());
+        tvBrief.setText(data.getGoodsBrief());
+        tvCurrencyPrice.setText(data.getCurrencyPrice());
+        tvName.setText(data.getGoodsName());
     }
 }
